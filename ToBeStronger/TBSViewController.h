534c7c8fc700f8b5cb1a040387c05b71bf4187d2
Zhang Boxuan <benny@ZhangtekiMacBook-Pro.local>
2014-03-07 12:29:46 -0500

    Initial Commit

---
+++ ToBeStronger/TBSViewController.h
@@ -0,0 +1,13 @@
+//
+//  TBSViewController.h
+//  ToBeStronger
+//
+//  Created by Zhang Boxuan on 14-3-7.
+//  Copyright (c) 2014年 Boxuan Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TBSViewController : UIViewController
+
+@end
---
+++ ToBeStronger/TBSViewController.m
@@ -0,0 +1,29 @@
+//
+//  TBSViewController.m
+//  ToBeStronger
+//
+//  Created by Zhang Boxuan on 14-3-7.
+//  Copyright (c) 2014年 Boxuan Zhang. All rights reserved.
+//
+
+#import "TBSViewController.h"
+
+@interface TBSViewController ()
+
+@end
+
+@implementation TBSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
